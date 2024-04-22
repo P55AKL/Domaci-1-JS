f1afc4e9060de432520f8711c891ecb0dc30dc10
P55AKL <167803042+P55AKL@users.noreply.github.com>
2024-04-22 19:30:43 +0200

    Add files via upload

---
+++ java domaci 1.js
@@ -0,0 +1,53 @@
+ /*ZADATAK 5 
+
+ let broj = 10;
+ for (i=0;i<=broj;i++)  {
+    console.log (i) ;
+} 
+/* ZADATAK 5 WHILE */
+/*
+let i = 0;
+ while (i<10)  {
+    console.log (i++); } 
+*/
+
+/* ZADATAK 6 */
+/*
+const broj =  [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20] 
+const len = broj.length;
+for (let i = 0; i < len ; i++) {
+    if (broj[i] % 3 === 0 )  console.log (i); } 
+    */
+   /* ZADATAK 7 */
+   /*
+   let product = 1;
+   for ( let i = 4; i <= 8; i++)  {
+    product *=i;
+} console.log(product);
+*/
+/* ZADATAK 8 */ /*
+for (let i =1; i<=15;i+=2) { 
+    console.log (i); } */
+    /* ZADATAK 9 */
+/*
+    let brojevi = [1,6,2,4,8]
+    for ( i=0;i<brojevi.length;i++) {
+console.log (brojevi[i]) } */
+/* ZADATAK 10 */ /*
+for ( let i =10; i>=1; i--) {
+    console.log(i)
+} */
+/* Zadatak 11 */ /*
+for ( let i =1; i<=5; i++)  { 
+    for (let a = 1;a<=5; a++) {
+        console.log ( i + " x " + a + " = " + i *a); }
+    }
+*/
+/* Zadatak 12 */
+/*
+const myArray = [2, "pozdrav", true, 5, "programiranje"];
+for (let i = 0; i < myArray.length; i++) {
+    if (typeof myArray[i] === "string") {
+        console.log(myArray[i]);
+    }
+}*/
